659df398291fe130e60749e9a89f70e78d795eae
Valery Moiseev <vamotest@gmail.com>
2020-05-20 10:33:23 +0300

    Add positive and negative tests

--- bitly/test_script.py
+++ bitly/test_script.py
@@ -1,26 +1,56 @@
-from .script import main
+from .script import return_token, return_bitly, shorten_link, count_clicks
 import pytest
 
 
-@pytest.mark.parametrize('user_input_url', [
+@pytest.mark.parametrize('user_input', [
 	"http://google.com",
-	"https://google.com",
-	"http://bit.ly/3d5pjun",
-	"https://bit.ly/2y6XJ1f",
+	"https://google.com"
 ])
-def test_qwe(user_input_url):
+def test_shorten_link_positive(user_input):
 
-	# if 'http' or 'https' not in user_input_url:
-	# 	user_input = f'https://{user_input_url}'
-	# 	result = main(user_input)
-	# else:
-	# 	result = main(user_input_url)
+	token = return_token()
+	short_link = shorten_link(user_input, token)
+	print(short_link)
 
-	result = main(user_input_url)
+	assert short_link.startswith('Короткая ссылка')
 
-	if 'Короткая ссылка:' in result:
-		assert True
-	elif 'По вашей ссылке прошли' in result:
-		assert True
-	else:
-		assert False
+
+@pytest.mark.parametrize('user_input', [
+	"google"
+])
+def test_shorten_link_negative(user_input):
+
+	token = return_token()
+	short_link = shorten_link(user_input, token)
+	print(short_link)
+
+	assert short_link.startswith('\nHTTP Error occured')
+
+
+@pytest.mark.parametrize('user_input', [
+	"http://bit.ly/3d5pjun",
+	"https://bit.ly/2y6XJ1f"
+])
+def test_count_clicks_positive(user_input):
+
+	token = return_token()
+	bitlink = return_bitly(user_input)[0]
+
+	total_clicks = count_clicks(bitlink, token)
+	print(total_clicks)
+
+	assert total_clicks.startswith('По вашей ссылке прошли')
+
+
+@pytest.mark.parametrize('user_input', [
+	"http://bit.ly/5dc76c61-4291-429b-9111-bcbe73621eef",
+	"https://bit.ly/dc4ed5c0-05b9-4558-937e-2fba1c9119af"
+])
+def test_count_clicks_negative(user_input):
+	token = return_token()
+	bitlink = return_bitly(user_input)[0]
+
+	total_clicks = count_clicks(bitlink, token)
+	print(total_clicks)
+
+	assert total_clicks.startswith('\nHTTP Error occured')
